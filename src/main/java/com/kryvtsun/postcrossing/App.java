909e1e33670d47920ae11d2f3080a6dbab5c01dc
Andriy Kryvtsun <andriy.kryvtsun@dataart.com>
2022-12-30 17:43:01 +0200

    renaming package

--- src/main/java/com/kryvtsun/postcrossing/App.java
+++ src/main/java/com/kryvtsun/postcrossing/App.java
@@ -1,10 +1,10 @@
-package com.kryvtsun;
+package com.kryvtsun.postcrossing;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/test/java/com/kryvtsun/postcrossing/AppTest.java
+++ src/test/java/com/kryvtsun/postcrossing/AppTest.java
@@ -1,8 +1,8 @@
-package com.kryvtsun;
-
-import static org.junit.Assert.assertTrue;
+package com.kryvtsun.postcrossing;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
